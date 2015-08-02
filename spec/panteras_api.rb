610b3d18f99a77e543962a1e910d63ac27c67fe1
Jonathan Colby <jcolby@team.mobile.de>
2015-08-02 22:27:11 +0200

    rspec playing around

--- spec/panteras_api.rb
+++ spec/panteras_api.rb
@@ -1,11 +1,8 @@
-require 'spec_helper'
+require_relative 'spec_helper'
 
 describe PanterasApi do
   it 'has a version number' do
     expect(PanterasApi::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
